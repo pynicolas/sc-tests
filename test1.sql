2ad8d3c3ad45b8a48a6bc386d0c521c93beebaa2
pynicolas <pierre-yves.nicolas@sonarsource.com>
2022-10-03 15:48:59 +0200

    Update test1.sql

--- test1.sql
+++ test1.sql
@@ -1,3 +1,4 @@
+-- Test for reserved words
 create table begin ( loop number, end number);
 insert into begin values (1, 1);
 select loop as continue, end as while from begin if where loop=1 and loop=2;

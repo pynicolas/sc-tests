972cf86c6ac2769f65b4bcf68d2006829c1f7763
pynicolas <pierre-yves.nicolas@sonarsource.com>
2023-10-03 15:28:42 +0200

    Update test1.sql

--- test1.sql
+++ test1.sql
@@ -3,3 +3,5 @@ create table begin ( loop number, end number);
 insert into begin values (1, 1);
 select loop as continue, end as while from begin if where loop=1 and loop=2;
 select xoop as xontinue, xnd as xhile from xegin xf where xoop=1 and xoop=2;
+
+
